7e645974437055a7a7df56eba8ed6064309269e4
claudia034 <000372216@uca.esu.sv>
2023-05-22 14:47:10 -0600

    :rocket: adaptador recycleView

--- app/src/main/java/com/example/parcial_programacionmoviles/Adapter/RestaurantRecycleViewAdapter.kt
+++ app/src/main/java/com/example/parcial_programacionmoviles/Adapter/RestaurantRecycleViewAdapter.kt
@@ -1,9 +1,33 @@
 package com.example.parcial_programacionmoviles.Adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.parcial_programacionmoviles.model.RestaurantModel
 
-//class RestaurantRecycleViewAdapter (
-    //private val clickListener: (RestaurantModel) -> Unit
-//): RecyclerView.Adapter<RestaurantRecycleViewAdapter>
+class RestaurantRecycleViewAdapter (
+    private val clickListener: (RestaurantModel) -> Unit
+): RecyclerView.Adapter<RestaurantRecyclerViewHolder>() {
+    private val restaurant = ArrayList<RestaurantModel>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RestaurantRecyclerViewHolder {
+        val binding = FragmentItemRestaurantBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return RestaurantRecyclerViewHolder(binding)
+    }
+
+    override fun getItemCount() = restaurant.size
+
+    override fun onBindViewHolder(holder: RestaurantRecyclerViewHolder, position: Int) {
+        val restaurant = restaurant[position]
+        holder.bind(restaurant, clickListener)
+    }
+
+    fun setData(restaurantList: List<RestaurantModel>){
+        restaurant.clear()
+        restaurant.addAll(restaurantList)
+    }
+}
 
--- app/src/main/java/com/example/parcial_programacionmoviles/Adapter/RestaurantRecyclerViewHolder.kt
+++ app/src/main/java/com/example/parcial_programacionmoviles/Adapter/RestaurantRecyclerViewHolder.kt
@@ -1,4 +1,17 @@
 package com.example.parcial_programacionmoviles.Adapter
 
+import androidx.recyclerview.widget.RecyclerView
+import com.example.parcial_programacionmoviles.model.RestaurantModel
 
-//class RestaurantRecyclerViewHolder(private val binding: FragmentItemRestaurantBInding):
+
+class RestaurantRecyclerViewHolder(private val binding: FragmentItemRestaurantBinding): RecyclerView.ViewHolder(binding.root) {
+
+    fun bind (
+        restaurant: RestaurantModel, clickListener: (RestaurantModel) -> Unit
+    ){
+        binding.textViewName.text = restaurant.name + " " + restaurant.tipo + " " + restaurant.description
+        binding.cardItem.setOnCLickListener{
+            clickListener(restaurant)
+        }
+    }
+}
